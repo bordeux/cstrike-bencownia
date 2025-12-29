0a120057e88425ab6915fb3f030d10dc7b6fd5f3
Chris Bednarczyk <chris@tourradar.com>
2025-12-29 20:21:34 +0100

    Remove mentions

---
+++ entrypoint.sh.d/99-persistent-storage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Links folders to persistent storage
+
+if [ "${AMXMODX_AUTOCOMPILE:-1}" = "1" ]; then
+    AMXMODX_PATH="${CSTRIKE_PATH}/addons/amxmodx"
+
+    # Use the helper script to compile plugins
+    "${HELPERS_PATH}/amxmodx-compile.sh" "$AMXMODX_PATH"
+else
+    echo "AMXMODX auto-compile disabled (AMXMODX_AUTOCOMPILE=${AMXMODX_AUTOCOMPILE})"
+fi
